8a8a8d01ca72ab0e9188c6e940f68d1f061a64a9
Bernardo Neres <90685959+beneres@users.noreply.github.com>
2023-03-28 18:03:06 +0100

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class MyModel(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+
+    @staticmethod
+    def search(query):
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM django_migrations WHERE name = %s", [query])
+            results = cursor.fetchall()
+            return results
